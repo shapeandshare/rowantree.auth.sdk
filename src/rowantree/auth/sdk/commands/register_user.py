44aa1b782ac6b6a1326341c38981108ea13db650
Joshua Burt <joshburt@shapeandshare.com>
2022-09-10 10:31:50 -0700

    register user

--- src/rowantree/auth/sdk/commands/register_user.py
+++ src/rowantree/auth/sdk/commands/register_user.py
@@ -1,40 +1,40 @@
-""" Action Queue Process Command Definition """
+""" Register User Command Definition """
 
 from starlette import status
 
 from rowantree.common.sdk import demand_env_var
 
-from src.rowantree.auth.sdk.contracts.requests.authenticate_user_request import AuthenticateUserRequest
 from ..contracts.dto.request_status_codes import RequestStatusCodes
 from ..contracts.dto.token import Token
 from ..contracts.dto.wrapped_request import WrappedRequest
 from ..contracts.request_verb import RequestVerb
 from .abstract_command import AbstractCommand
+from ..contracts.requests.register_user_request import RegisterUserRequest
 
 
-class AuthenticateUserCommand(AbstractCommand):
+class RegisterUserCommand(AbstractCommand):
     """
-    Authenticate User Command
+    Register User Command
 
     Methods
     -------
-    execute(self, request: AuthenticateUserRequest) -> None
-        Executes the command.
+    execute(self, request: User) -> None
+        Register User
     """
 
-    def execute(self, request: AuthenticateUserRequest) -> Token:
+    def execute(self, request: RegisterUserRequest) -> Token:
         """
-        Executes the command.
+        Register User
 
         Parameters
         ----------
-        request: AuthenticateUserRequest
+        request: RegisterUserRequest
         """
 
         request: WrappedRequest = WrappedRequest(
             verb=RequestVerb.POST,
-            url=f"{demand_env_var(name='ACCESS_AUTH_ENDPOINT')}/v1/auth/token",
-            statuses=RequestStatusCodes(allow=[status.HTTP_200_OK], reauth=[status.HTTP_401_UNAUTHORIZED], retry=[]),
+            url=f"{demand_env_var(name='ACCESS_AUTH_ENDPOINT')}/v1/auth/register",
+            statuses=RequestStatusCodes(allow=[status.HTTP_200_OK], reauth=[], retry=[]),
             data=request.dict(),
         )
         response: dict = self.wrapped_request(request=request)
